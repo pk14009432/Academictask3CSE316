d69adc21eea8e8b673a6996bf6287a1fd2e19207
Pintu Kumar <43597083+pk14009432@users.noreply.github.com>
2020-04-09 01:36:53 +0530

    Update forkchild.c

--- forkchild.c
+++ forkchild.c
@@ -1,40 +1,48 @@
-#include <stdio.h>  
-#include <stdlib.h>  
-#include <unistd.h>  
-  
-int main()  
-{  
-int pid, pid1, pid2;  
- 
-pid = fork();  
- 
-if (pid == 0) {  
-  
- sleep(3);  
-  
- printf("child[1] --> pid = %d and ppid = %d\n",  
-  getpid(), getppid());  
-}  
-else {  
-pid1 = fork();  
- if (pid1 == 0) {  
-  sleep(2);  
-  printf("child[2] --> pid = %d and ppid = %d\n", getpid(), getppid());  
- }  
- else {  
-  pid2 = fork();  
-  if (pid2 == 0) {  
-  
-   printf("child[3] --> pid = %d and ppid = %d\n",  
-    getpid(), getppid());  
-  }  
-  
-  else {  
-   
-   sleep(3);  
-   printf("parent --> pid = %d\n", getpid());  
-  }  
- }  
-}  
-return 0;  
-}  
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+
+	int n=0;
+	int k=0;
+	
+	pid_t pid;
+
+		do
+		{
+			printf("Please enter a number greater than 0\n"); 
+  			scanf("%d", &k);	
+		}while (k <= 0);
+
+		pid = fork();
+
+		if (pid == 0)
+		{
+			printf("Child is working...\n");
+			printf("%d\n",k);
+			while (k!=1)
+			{
+				if (k%2 == 0)
+				{
+					k = k/2;
+				}
+				else if (k%2 == 1)
+				{
+					k = 3 * (k) + 1;
+				}	
+			
+				printf("%d\n",k);
+			}
+		
+			printf("Child process is done.\n");
+		}
+		else
+		{
+			printf("Parents is waiting on child process...\n");
+			wait();
+			printf("Parent process is done.\n");
+		}
+	return 0; 
+}
